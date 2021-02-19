d33d1360a2ecd37a15b2962ae2b0adc0180a55d9
Tobias Heel <heeltobias@gmail.com>
2021-02-19 10:31:02 +0100

    Add chromedriver to app directory

--- Installer/requirements.ps1
+++ Installer/requirements.ps1
@@ -3,4 +3,3 @@ Set-ExecutionPolicy Unrestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install choco install googlechrome -y 
 choco install dotnet4.7.2 -y
-choco install chromedriver -y
