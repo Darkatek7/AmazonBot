1eba3d0cadc7148ab0fe9dce36fb1d710a47dce6
Tobias Heel <heeltobias@gmail.com>
2021-07-26 14:08:04 +0200

    Update the Bot

--- Installer/Requirements/requirements.ps1
+++ Installer/Requirements/requirements.ps1
@@ -1,5 +1,8 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 Set-ExecutionPolicy Unrestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install googlechrome --version=89.0.4389.114 -y
+choco install googlechrome -y
+choco upgrade googlechrome -y
+choco install chromedriver -y
+choco upgrade chromedriver -y
 choco install dotnet-5.0-sdk -y
