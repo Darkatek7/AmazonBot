f368c8f245d2cd2db9fbc7030b93250c5c91395b
Darkatek7 <heeltobias@gmail.om>
2022-01-04 14:15:56 +0100

    Remove Version Specifics

--- Installer/Requirements/requirements.ps1
+++ Installer/Requirements/requirements.ps1
@@ -1,6 +1,6 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 Set-ExecutionPolicy Unrestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install googlechrome --version=92.0.4515.159 --force -y
-choco install chromedriver --version=92.0.4515.1070 --force -y
+choco install googlechrome --force -y
+choco install chromedriver --force -y
 choco install dotnet-5.0-sdk -y
